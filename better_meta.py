6c40c7cc581314dc406fb5073390b7cfc801963d
Klaus Laube <klaus@corp.globo.com>
2014-04-01 00:10:59 -0300

    Resolving default values when the meta information doesn't exist

--- better_meta.py
+++ better_meta.py
@@ -1,24 +1,42 @@
 from pelican import signals
 
 META_ATTRIBUTES = ('description', 'keywords', 'robots')
+DEFAULT_ROBOTS = 'index,follow'
 
 
 def register():
-    signals.article_generator_finalized.connect(init_better_meta)
+    signals.article_generator_finalized.connect(BetterMeta.add_meta_to_articles)
 
 
-def init_better_meta(generator):
-    for article in generator.articles:
-        create_meta_attribute(article)
+class BetterMeta:
 
+    @classmethod
+    def add_meta_to_articles(cls, generator):
+        for article in generator.articles:
+            cls.create_meta_attribute(article)
 
-def create_meta_attribute(article):
-    article.meta = {}
+    @classmethod
+    def create_meta_attribute(cls, article):
+        article.meta = {}
 
-    for key in META_ATTRIBUTES:
-        article_attrib = "meta_%s" % key
+        for key in META_ATTRIBUTES:
+            article_attrib = "meta_%s" % key
 
-        if not hasattr(article, article_attrib):
-            continue
+            if hasattr(article, article_attrib):
+                meta_value = getattr(article, article_attrib)
+            else:
+                meta_value = getattr(cls, "get_default_%s" % article_attrib)(article)
 
-        article.meta[key] = getattr(article, article_attrib)
+            article.meta[key] = meta_value
+
+    @classmethod
+    def get_default_meta_description(cls, article):
+        return article.summary
+
+    @classmethod
+    def get_default_meta_keywords(cls, article):
+        return ', '.join([tag.slug for tag in article.tags])
+
+    @classmethod
+    def get_default_meta_robots(cls, article):
+        return DEFAULT_ROBOTS
--- test_better_meta.py
+++ test_better_meta.py
@@ -1,6 +1,6 @@
 import unittest
 import better_meta
-from better_meta import init_better_meta, create_meta_attribute
+from better_meta import BetterMeta
 
 
 class FakeGenerator(object):
@@ -15,47 +15,58 @@ class FakeGenerator(object):
 
 
 class FakeArticle(object):
-    pass
+
+    def __init__(self):
+        self.summary = 'should-be-summary'
+        self.tags = ['tag1', 'tag2', 'tag3']
 
 
-class BetterMetaFilledMetaTestCase(unittest.TestCase):
+class BetterMetaTestCase(unittest.TestCase):
 
     def setUp(self):
         self.article = FakeArticle()
 
+
+class BetterMetaFilledMetaTestCase(BetterMetaTestCase):
+
     def test_should_create_meta_attribute_on_article_when_it_has_description(self):
         self.article.meta_description = 'should-be-meta-description'
 
-        create_meta_attribute(self.article)
+        BetterMeta.create_meta_attribute(self.article)
 
         self.assertEqual(self.article.meta['description'], 'should-be-meta-description')
 
     def test_should_create_meta_attribute_on_article_when_it_has_keywords(self):
         self.article.meta_keywords = 'should-be-meta-keywords'
 
-        create_meta_attribute(self.article)
+        BetterMeta.create_meta_attribute(self.article)
 
         self.assertEqual(self.article.meta['keywords'], 'should-be-meta-keywords')
 
     def test_should_create_meta_robots_on_article_when_it_has_robots(self):
         self.article.meta_robots = 'index,follow'
 
-        create_meta_attribute(self.article)
+        BetterMeta.create_meta_attribute(self.article)
 
         self.assertEqual(self.article.meta['robots'], 'index,follow')
 
 
-class BetterMetaUnfilledMetaTestCase(unittest.TestCase):
+class BetterMetaUnfilledMetaTestCase(BetterMetaTestCase):
 
     def setUp(self):
-        self.article = FakeArticle()
+        super(BetterMetaUnfilledMetaTestCase, self).setUp()
+        BetterMeta.create_meta_attribute(self.article)
 
-    def test_should_create_meta_attribute_as_a_empty_dict(self):
-        create_meta_attribute(self.article)
-        self.assertEqual(self.article.meta, {})
+    def test_should_use_the_summary_as_description_when_it_hasnt_meta_description(self):
+        self.assertEqual(self.article.meta['description'], 'should-be-summary')
 
+    def test_should_use_tags_as_keywords_when_it_hasnt_meta_keywords(self):
+        self.assertEqual(self.article.meta['keywords'], 'tag1, tag2, tag3')
 
-class BetterMetaTestCase(unittest.TestCase):
+    def test_should_use_index_follow_as_default_robots_data(self):
+        self.assertEqual(self.article.meta['robots'], 'index,follow')
+
+class BetterMetaPelicanIntegrationTestCase(unittest.TestCase):
 
     def setUp(self):
         self.generator = FakeGenerator()
@@ -64,7 +75,7 @@ class BetterMetaTestCase(unittest.TestCase):
         self.assertTrue(hasattr(better_meta, 'register'))
 
     def test_should_create_meta_attribute_with_data_on_articles(self):
-        init_better_meta(self.generator)
+        BetterMeta.add_meta_to_articles(self.generator)
 
         for article in self.generator.articles:
             self.assertEqual(article.meta['description'], 'should-be-description')
